c63aa5882dbcffdadea18b06a99c90986fda0dcc
webew <fillion.bertrand@gmail.com>
2022-03-03 10:50:01 +0100

    entity commentaire+User

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Article;
 use App\Entity\Categorie;
+use App\Entity\Commentaire;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,18 +12,32 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $cat = new Categorie();
-        $cat->setNom("Nouvelles technologies");
-        $manager->persist($cat);
+        for ($i = 1; $i <= 5; $i++) {
+            $cat = new Categorie();
+            $cat->setNom("Nouvelle categorie " . $i);
+            $manager->persist($cat);
 
-        $article = new Article();
-        $article->setTitre("Les ordinateurs en bois");
-        $article->setContenu("Lorem, ipsum dolor sit amet consectetur adipisicing elit.");
-        $article->setDatePublication(new \DateTime());
-        $article->setImageSrc("toto.jpg");
-        $article->setNombreVues(0);
-        $article->setCategorie($cat);
-        $manager->persist($article);
+            for ($j = 1; $j <= 10; $j++) {
+                $article = new Article();
+                $article->setTitre("Nouvel article n°" . $j);
+                $article->setContenu("Lorem, ipsum dolor sit amet consectetur adipisicing elit.");
+                $article->setDatePublication(new \DateTime());
+                $article->setImageSrc("toto.jpg");
+                $article->setNombreVues(0);
+                $article->setCategorie($cat);
+                $manager->persist($article);
+
+                for ($k = 1; $k <= 5; $k++) {
+                    $commentaire = new Commentaire();
+                    $commentaire->setDate(new \DateTime());
+                    $commentaire->setContenu("Super article !" . $k);
+                    $commentaire->setPublie(false);
+                    $commentaire->setArticle($article);
+                    $manager->persist($commentaire);
+                }
+            }
+        }
+
 
         $manager->flush();
     }
