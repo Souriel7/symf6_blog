56566e2d4f2d2b86f22ebd18a91e2d669670deaf
webew <fillion.bertrand@gmail.com>
2022-03-07 11:38:21 +0100

    form commentaire

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Commentaire;
+use App\Entity\User;
+use App\Form\CommentaireType;
 use App\Repository\ArticleRepository;
+use App\Repository\UserRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,12 +26,30 @@ class MainController extends AbstractController
     }
 
     #[Route('/articles/{id}', name: 'app_articles_id')]
-    public function getArticleById($id, ArticleRepository $articleRepository)
+    public function getArticleById($id, ArticleRepository $articleRepository, Request $request, UserRepository $userRepository, ManagerRegistry $managerRegistry)
     {
         $article = $articleRepository->find($id);
 
+        $commentaireForm = $this->createForm(CommentaireType::class);
+
+        $commentaireForm->handleRequest($request);
+        if ($commentaireForm->isSubmitted() && $commentaireForm->isValid()) {
+            $commentaire = $commentaireForm->getData();
+            $commentaire->setDate(new \DateTime());
+            $commentaire->setPublie(false);
+            $commentaire->setArticle($article);
+            $user = $userRepository->find(8);
+            $commentaire->setUser($user);
+
+            $entityManager = $managerRegistry->getManager();
+            $entityManager->persist($commentaire);
+            $entityManager->flush();
+            // dd($commentaire);
+        }
+
         return $this->render('main/article.html.twig', [
-            "article" => $article
+            "article" => $article,
+            "commForm" => $commentaireForm->createView()
         ]);
     }
 }
